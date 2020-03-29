7039996aabe44d1a7123c14d354fc3662b3e6330
devsangho <gaonmail@cau.ac.kr>
2020-03-29 16:14:57 +0900

    Initial Commit

---
+++ algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by 윤상호 on 2020/03/29.
+//  Copyright © 2020 윤상호. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
